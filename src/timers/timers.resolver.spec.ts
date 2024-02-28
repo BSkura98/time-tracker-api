6290e4f1dbf9b05cc247f459e815f287cf42b248
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-02-28 12:25:40 +0100

    Create tests for TimersResolver

--- src/timers/timers.resolver.spec.ts
+++ src/timers/timers.resolver.spec.ts
@@ -1,11 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TimersResolver } from './timers.resolver';
 import { TimersService } from './timers.service';
+import { getRandomElement } from '../utils/getRandomElement';
+
+const getRandomId = () => Math.floor(Math.random() * 100000000);
+
+const timers = [
+  {
+    id: getRandomId(),
+    name: 'Cooking',
+  },
+  {
+    id: getRandomId(),
+    name: 'Cleaning',
+  },
+  {
+    id: getRandomId(),
+    name: 'Watching TV',
+  },
+  {
+    id: getRandomId(),
+    name: 'Working',
+  },
+];
 
 describe('TimersResolver', () => {
   let resolver: TimersResolver;
 
-  const mockTimersService = {};
+  const mockTimersService = {
+    createTimer: jest.fn((dto) => ({
+      id: Date.now(),
+      ...dto,
+    })),
+    findAll: jest.fn(() => timers),
+    findOne: jest.fn((id) => ({
+      id,
+      name: timers.find((timer) => timer.id === id).name,
+    })),
+    update: jest.fn((id, dto) => ({
+      id,
+      ...dto,
+    })),
+    remove: jest.fn((id) => ({
+      id,
+      name: timers.find((timer) => timer.id === id).name,
+    })),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,5 +60,46 @@ describe('TimersResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should create a timer', () => {
+    const dto = { name: 'Jogging' };
+
+    expect(resolver.createTimer(dto)).toEqual({
+      id: expect.any(Number),
+      name: dto.name,
+    });
+
+    expect(mockTimersService.createTimer).toHaveBeenCalledWith(dto);
+  });
+
+  it('should get a timer by id', () => {
+    const timer = getRandomElement(timers);
+
+    expect(resolver.findOne(timer.id)).toEqual(timer);
+
+    expect(mockTimersService.findOne).toHaveBeenCalledWith(timer.id);
+  });
+
+  it('should get timers', () => {
+    expect(resolver.findAll()).toEqual(timers);
+
+    expect(mockTimersService.findAll).toBeCalled();
+  });
+
+  it('should update a timer', () => {
+    const dto = { id: getRandomElement(timers).id, name: 'Doing nothing' };
+
+    expect(resolver.updateTimer(dto)).toEqual(dto);
+
+    expect(mockTimersService.update).toHaveBeenCalledWith(dto.id, dto);
+  });
+
+  it('should remove a timer', () => {
+    const timer = getRandomElement(timers);
+    console.log(timers);
+    expect(resolver.removeTimer(timer.id)).toEqual(timer);
+
+    expect(mockTimersService.remove).toHaveBeenCalledWith(timer.id);
   });
 });
