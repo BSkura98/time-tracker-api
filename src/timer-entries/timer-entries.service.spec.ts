6831c8927786803d9b659b541ece5666469da5bd
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-03-06 18:44:48 +0100

    Mock timer entries repository and timers service

--- src/timer-entries/timer-entries.service.spec.ts
+++ src/timer-entries/timer-entries.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
 import { TimerEntriesService } from './timer-entries.service';
+import { TimerEntry } from './entities/timer-entry.entity';
+import { TimersService } from '../timers/timers.service';
 
 describe('TimerEntriesService', () => {
   let service: TimerEntriesService;
 
+  const mockTimerEntriesRepository = {};
+
+  const mockTimersService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TimerEntriesService],
+      providers: [
+        TimerEntriesService,
+        {
+          provide: getRepositoryToken(TimerEntry),
+          useValue: mockTimerEntriesRepository,
+        },
+        {
+          provide: TimersService,
+          useValue: mockTimersService,
+        },
+      ],
     }).compile();
 
     service = module.get<TimerEntriesService>(TimerEntriesService);
